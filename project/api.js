daa89b7cbea1059a3e09479ad08c68de78042db3
stefanjaeger <stefanjaeger@bluewin.ch>
2015-07-28 13:42:14 +0200

    added cors header

--- project/api.js
+++ project/api.js
@@ -5,8 +5,10 @@
 
     exports.listen = function (port, query) {
         var app = express();
-
+  
         app.get('/discover/movie', function (req, res) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             res.type('application/json; charset=utf-8');
             var result = {
                 movies: query(req.query.runtimeFrom, req.query.runtimeTo)
