eff99b71a05cad308225c6e11ca9bddd52fbb59d
Stefan Jäger <stefanjaeger@bluewin.ch>
2015-07-30 14:06:27 +0200

    small refactorings

--- app.js
+++ app.js
@@ -11,33 +11,39 @@
         db = new loki('MovieDB.json'),
         movies;
 
-
     fs.exists(__dirname + '/MovieDB.json', function (exists) {
         if (exists) {
-            db.loadDatabase({}, function () {
-                movies = db.getCollection('movies');
-            });
-            console.log('sweet, we\'re going fast track and are up and running!');
+            loadAppFromPersistedDatabase();
         } else {
-            movies = db.addCollection('movies', {
-                indices: ['runtimeParsed']
-            });
-
-            parser.parse(__dirname + '/project/data/omdbMovies.txt', function (data) {
-                movies.insert(data);
-            }, function (data) {
-                return data.imdbVotes > 6.5 && data.poster !== '' && (data.language === 'English' || data.language === 'German');
-            }, function () {
-                console.log('sweet! all movies loaded');
-                db.saveDatabase();
-            });
+            parseMoviesFile();
         }
     });
-
 
     api.listen(process.env.PORT || 8080, config.apikey, function (runtimeFrom, runtimeTo) {
         return movies.chain().where(function (obj) {
             return obj.runtimeParsed > runtimeFrom && obj.runtimeParsed < runtimeTo;
         }).data();
     });
+
+    function loadAppFromPersistedDatabase() {
+        db.loadDatabase({}, function () {
+            movies = db.getCollection('movies');
+            console.log('sweet, we\'re going fast track and are up and running!');
+        });
+    }
+
+    function parseMoviesFile() {
+        movies = db.addCollection('movies', {
+            indices: ['runtimeParsed']
+        });
+
+        parser.parse(__dirname + config.originalMoviesFile, function (data) {
+            movies.insert(data);
+        }, function (data) {
+            return data.imdbVotes > 6.5 && data.poster !== '' && (data.language === 'English' || data.language === 'German');
+        }, function () {
+            console.log('sweet! all movies loaded');
+            db.saveDatabase();
+        });
+    }
 }());
--- project/api.js
+++ project/api.js
@@ -6,22 +6,26 @@
     exports.listen = function (port, apikey, query) {
         var app = express();
 
+        app.use('/discover/movie', function (req, res, next) {
+            if (!req.query.apikey || req.query.apikey !== apikey) {
+                res.send({
+                    movies: [],
+                    error: 'apikey wrong or missing'
+                });
+            } else {
+                next();
+            }
+        });
+
         app.get('/discover/movie', function (req, res) {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             res.type('application/json; charset=utf-8');
-
-            if (!req.query.apikey || req.query.apikey !== apikey) {
-                res.send({
-                    error: 'apikey wrong or missing'
-                });
-            } else {
-
-                var result = {
-                    movies: query(req.query.runtimeFrom, req.query.runtimeTo)
-                };
-                res.send(JSON.stringify(result));
-            }
+            
+            var result = {
+                movies: query(req.query.runtimeFrom, req.query.runtimeTo)
+            };
+            res.send(JSON.stringify(result));
         });
 
         app.listen(port);
